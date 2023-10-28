15c73d109b97427f8c7ab36d666070b133daa0b7
Mercy Muthoni Ndirangu <105234288+Sonnie-Mercy@users.noreply.github.com>
2023-10-28 18:20:45 +0300

    the footer

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -1,14 +1,32 @@
 //footer component
-import {rrfavicongreen,} from '../Assets'
+import {logocolor,BiLogoFacebook,BiLogoTwitter, BiLogoPinterest, BiLogoDribbble, BiLogoInstagramAlt, BiLogoYoutube} from '../Assets'
 function Footer() {
     return (
      <footer className='footer'>
-        <div>
-            <img src={rrfavicongreen} alt="favicon" />
-            <p>© 2023 Recipe-Rise. All rights reserved</p>
+        <div className="footer-top-area">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div id ="logo" className="widget">
+                            <img src={logocolor} width="199" height="58" alt='Logo'></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="socialmedia">
+            <ul className='socialmediacounter'>
+                <li className='xs-counter-li'><BiLogoFacebook /> fans</li>
+                <li className='xs-counter-li'><BiLogoTwitter /> followers</li>
+                <li className='xs-counter-li'><BiLogoPinterest /> followers</li>
+                <li className='xs-counter-li'><BiLogoDribbble /> followers</li>
+                <li className='xs-counter-li'><BiLogoInstagramAlt /> followers</li>
+                <li className='xs-counter-li'><BiLogoYoutube /> subscribers</li>
+            </ul>
+        </div>
+        <div class="copyright">© 2023 Recipe-Rise All rights reserved
         </div>
      </footer>
     );
 }
-
 export default Footer;

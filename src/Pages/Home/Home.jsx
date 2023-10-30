e84cdaeef0bf5fc2d789399042afde663d007499
unknown <wawerur95@gmail.com>
2023-10-30 13:07:38 +0300

    footer updates, faq/category setup

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,19 @@
 import {Wrapper } from "../../Layouts";
-import {Slider} from '../../Components'
+import {Slider, Category} from '../../Components'
+
 function Home() {
     return (
     <Wrapper>
         <div className="main">
         <Slider/>
+        {/* category */}
+         <div>
+            <Category title="Breakfast"/>
+            <Category title="Dinner"/>        
+            <Category/>
+         </div>
+        {/* end of category */}
         </div>
-      
     </Wrapper>
     );
 }
--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,2 +1,3 @@
 export {default as Home} from './Home/Home'
 export {default as Login} from './Auth/Login'
+export {default as Faq} from './FAQ/Faq'
